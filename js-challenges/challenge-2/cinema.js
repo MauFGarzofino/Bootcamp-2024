590af4dd6d258f2edc3a460130822e72851d9735
MauFGarzo <mauricioflores123321@gmail.com>
2024-07-12 18:30:11 -0400

    Added feature to generate dynamic row labels for cinema seats

--- js-challenges/challenge-2/cinema.js
+++ js-challenges/challenge-2/cinema.js
@@ -25,13 +25,26 @@ class Seat {
 }
 
 class Cinema {
-    constructor(columns, rows, rowLabels) {
+    constructor(columns, rows) {
         this.columns = columns;
         this.rows = rows;
-        this.rowLabels = rowLabels;
+        this.rowLabels = this.generateRowLabels(rows);
         this.seats = this.createSeats(columns, rows);
     }
 
+    generateRowLabels(rows) {
+        const labels = [];
+        const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        for (let i = 0; i < rows; i++) {
+            let label = '';
+            let index = i;
+            label = alphabet[index % 26] + label;
+            labels.push(label);
+        }
+        return labels;
+    }
+    
     createSeats(columns, rows) {
         const seats = [];
         for (let row = 0; row < rows; row++) {
@@ -79,8 +92,7 @@ class Cinema {
     }
 }
 
-const rowLabels = ['A', 'B', 'C', 'D', 'E'];
-const cine = new Cinema(5, 5, rowLabels);
+const cine = new Cinema(4, 45);
 cine.reserveSeat(2, 2);
 cine.displaySeats();
 cine.reserveSeat(0, 0);
